e7730344ff880af4f41883b721d6b0e9d4503192
unknown <G00260547@gmit.ie>
2021-03-18 23:49:09 +0000

    Navbar UI, add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import NavigationBar from './components/navigationBar';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="NavigationBar">
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
-          <Nav className="NavLinks">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/">Features</Nav.Link>
-            <Nav.Link href="/">Other</Nav.Link>
-          </Nav>
-        </Navbar>
-        {/* Links HERE */}
-      </div>
-      <div>
-        <h1>Hello World!!</h1>
-      </div>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        loggedIn: true
+    };
+
+    render() {
+        return (
+            // JSX (JavaScript XML) - Babel(A JavaScript compiler) converts JSX to JavaScript
+            <div className="App">
+                <NavigationBar
+                    loggedIn={this.state.loggedIn}>
+                </NavigationBar>
+                <div>
+                    <h1>Hello World!!!</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/navigationBar.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Cart4 } from 'react-bootstrap-icons';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+
+class NavigationBar extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="NavigationBar">
+                <BrowserRouter>
+                    <Navbar className="d-flex align-items-center" bg="dark" variant="dark" sticky="top">
+                        <Container>
+                            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                            <Nav className="NavLinks ">
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="/">Page_One</Nav.Link>
+                                <Nav.Link href="/">Page_Two</Nav.Link>
+                            </Nav>
+                            <Navbar.Collapse className="justify-content-end">
+                                <Nav>
+                                    {this.displayUserOrGuest()}
+                                    <Nav.Link href="/"><Cart4 color="white" size={24}></Cart4></Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                    <Switch>
+                        <Route path="/"></Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
+
+    displayUserOrGuest() {
+        if (this.props.loggedIn) {
+            return (
+                <NavDropdown title="UserName">
+                    <NavDropdown.Item href="/">Your Orders</NavDropdown.Item>
+                    <NavDropdown.Item href="/">Logout</NavDropdown.Item>
+                </NavDropdown>
+            )
+        } else {
+            return (
+                <Button variant="outline-light">Login</Button>
+            );
+        }
+    }
+}
+
+export default NavigationBar;
