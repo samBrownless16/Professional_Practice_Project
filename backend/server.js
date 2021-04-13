6f65148a948a576a88a87d9292d326e7bd792894
unknown <G00260547@gmit.ie>
2021-04-13 14:12:24 +0100

    add server calls for store pages

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,24 @@ const port = 4000;
 const mysql = require('mysql');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-var connection = mysql.createConnection({
+//var connection = mysql.createConnection({
+var connection = mysql.createPool({
+	connectionLimit: 20,
     host: '',
     user: 'customer',
     password: '',
     database: "storeDB"
+});
+
+// CORS
+app.use(cors());
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 // parse application
@@ -18,8 +30,22 @@ app.listen(port, () => {
     console.log("App Listening at Port " + port)
 });
 
-app.get('/', function(req, res) {
-	connection.query('SELECT * from Users', function (error, data) {
+app.get('/loginattempt', function(req, res) {
+    connection.query('SELECT username, pw from users WHERE username = ? AND pw = ?', [req.query.user, req.query.pw], function (error, data) {
+		if (error) throw error;
+		res.json(data);
+	});
+});
+
+app.get('/allproducts', function(req, res) {
+	connection.query('SELECT * from products', function (error, data) {	
+		if (error) throw error;
+		res.json(data);
+	});
+});
+
+app.get('/view_product/:id', function(req, res) {
+	connection.query('SELECT * from products WHERE productID = ?', [req.params.id], function (error, data) { 
 		if (error) throw error;
 		res.json(data);
 	});
