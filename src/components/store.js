91a47733c5b781349aba5886acc5f42b9180accc
unknown <G00260547@gmit.ie>
2021-04-23 21:20:59 +0100

    Fix Store Search issue, had to click Search button twice

--- src/components/store.js
+++ src/components/store.js
@@ -6,20 +6,20 @@ const port = 4000;
 export class Store extends Component {
     state = { storeProducts: [], productSearch: false }
 
-    /* Need a DidUpdate here as store page will not update if a search is performed while  
-       already on the store page i.e. the Store component is already mounted */
-    componentDidUpdate() {
+    componentDidMount() {
         // Check if a search has been performed (by checking if the param search is undefined or not)
-        if (this.props.match.params.search === undefined) {
-            axios.get(`http://localhost:${port}/allproducts`)
-            .then(response => {
-                this.setState({storeProducts: response.data});
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        } else {
-            // search param found so call server to find items matching the search
+        axios.get(`http://localhost:${port}/allproducts`)
+        .then(response => {
+            this.setState({storeProducts: response.data});
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    // Update store page if a search is performed (Update the already mounted component)
+    componentDidUpdate() {
+        if (this.props.match.params.search !== undefined) {
             axios.get(`http://localhost:${port}/product_search/` + this.props.match.params.search)
             .then(response => {
                 this.setState({storeProducts: response.data, productSearch: true});
